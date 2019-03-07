95fb3ffc8a1b24d0a1b29b4a52f1837ed0404cc8
delacruz1 <32399546+delacruz1@users.noreply.github.com>
2019-03-06 16:37:12 -0800

    edits to frontend, trying to get pusher to work

--- amazonAlexa/index.js
+++ amazonAlexa/index.js
@@ -1,7 +1,25 @@
+//Pusher API Dependencies
+
+const Pusher = require("pusher");
+
+let pusher = new Pusher({
+    appId: "730027",
+    key: "0d9a535805e414a1933f",
+    secret: "5d8978c00aaa349693ac",
+    cluster:  "us3",
+    encrypted: true
+
+});
+
+pusher.trigger('my-channel', 'my-event', {message: "Hello"});
+
+
+// Alexa dependencies
 const alexaSDK = require('alexa-sdk');
 const awsSDK = require('aws-sdk');
 const promisify = require('es6-promisify');
 
+// TODO but not really important for now: add any app id keys into a text file
 const appId = 'amzn1.ask.skill.9c63352f-f135-4ebb-90a9-993c304c72ae';
 const testFormTable = 'TestForm';
 const docClient =  new awsSDK.DynamoDB.DocumentClient(); //new AWS.DynamoDB.DocumentClient();
